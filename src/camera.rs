0d611576d65cd2ef438f6a95483289fb0ef0e98b
Juho Naatula <juhotuho10@gmail.com>
2024-05-11 13:55:09 +0300

    fully implemented the camera struct

--- src/camera.rs
+++ src/camera.rs
@@ -1,41 +1,161 @@
-use glam::{vec3a, Vec3A};
+use core::time;
 
-struct Camera {
-    pub position: glam::vec3a,
-    pub direction: glam::vec3a,
-    pub ray_direction: Vec<glam::vec3a>,
+use egui::Context;
+use egui_winit_platform::Platform;
+use glam::{mat4, quat, vec2, vec3a, vec4, Mat4, Quat, Vec2, Vec3A};
+
+pub struct Camera {
+    pub position: Vec3A,
+    pub direction: Vec3A,
+    pub ray_directions: Vec<Vec3A>,
+
     near_clip: f32,
     far_clip: f32,
     vertical_fov: f32,
-    speed: f32,
+
+    viewport_width: u32,
+    viewport_height: u32,
+
+    movement_speed: f32,
+    turning_speed: f32,
+
+    projection: Mat4,
+    inverse_projection: Mat4,
+    view: Mat4,
+    inverse_view: Mat4,
 }
 
 impl Camera {
-    fn new() -> Camera {
+    pub fn new() -> Camera {
         Camera {
-            position: glam::vec3a(0., 0., 3.),
-            direction: glam::vec3a(0., 0., -1.),
-            ray_direction: vec![],
+            position: vec3a(0., 0., 3.),
+            direction: vec3a(0., 0., -1.),
+            ray_directions: vec![],
+
+            viewport_width: 100,
+            viewport_height: 100,
+
             near_clip: 0.1,
             far_clip: 100.0,
             vertical_fov: 45.0,
-            speed: 0.1,
+
+            movement_speed: 0.1,
+            turning_speed: 0.3,
+
+            projection: Mat4::from_cols_slice(&[1.0; 16]),
+            inverse_projection: Mat4::from_cols_slice(&[1.0; 16]),
+            view: Mat4::from_cols_slice(&[1.0; 16]),
+            inverse_view: Mat4::from_cols_slice(&[1.0; 16]),
         }
     }
 
-    fn on_update(&self, timestep: f32) {}
+    pub fn on_update(&mut self, timestep: &f32, egui_context: &Context) -> bool {
+        let up_direction = glam::Vec3A::Y;
 
-    fn foward(&mut self) {
-        self.position += self.direction.normalize() * self.speed;
+        let right_direction = self.direction.cross(up_direction);
+
+        let mut moved: bool = false;
+
+        let mouse_delta = egui_context.input(|i: &egui::InputState| i.pointer.delta());
+
+        egui_context.input(|input: &egui::InputState| {
+            if input.key_down(egui::Key::W) {
+                // holding W
+                self.position += timestep * self.movement_speed * self.direction;
+                moved = true;
+            } else if input.key_down(egui::Key::S) {
+                // holding S
+                self.position -= timestep * self.movement_speed * self.direction;
+                moved = true;
+            }
+
+            if input.key_down(egui::Key::A) {
+                // holding A
+                self.position += timestep * self.movement_speed * right_direction;
+                moved = true;
+            } else if input.key_down(egui::Key::D) {
+                // holding D
+                self.position -= timestep * self.movement_speed * right_direction;
+                moved = true;
+            }
+        });
+
+        if mouse_delta.x != 0.0 || mouse_delta.y != 0.0 {
+            // rotate the camera
+            let pitch_delta: f32 = mouse_delta.y * self.turning_speed;
+            let yaw_delta: f32 = mouse_delta.x * self.turning_speed;
+
+            let right_rotation = Quat::from_axis_angle(right_direction.into(), -pitch_delta);
+            let up_rotation = Quat::from_axis_angle(up_direction.into(), -yaw_delta);
+
+            let q = (right_rotation * up_rotation).normalize();
+            self.direction = q.mul_vec3(self.direction.into()).into();
+
+            moved = true;
+        }
+
+        if moved {
+            self.recalculate_view();
+        }
+
+        moved
     }
 
-    fn backward(&mut self) {
-        self.position -= self.direction.normalize() * self.speed;
+    fn recalculate_projection(&mut self) {
+        let fov_rad = self.vertical_fov.to_radians();
+        let aspect_ratio = (self.viewport_width / self.viewport_height) as f32;
+        self.projection =
+            Mat4::perspective_rh_gl(fov_rad, aspect_ratio, self.near_clip, self.far_clip);
+
+        self.inverse_projection = self.projection.inverse();
     }
-    fn left(&mut self) {
-        self.position += glam::vec3a(0., self.speed, 0.)
+
+    fn recalculate_view(&mut self) {
+        self.view = Mat4::look_at_lh(
+            self.position.into(),
+            (self.position + self.direction).into(),
+            glam::Vec3::Y,
+        );
+        self.inverse_view = self.view.inverse();
     }
-    fn right(&mut self) {
-        self.position -= glam::vec3a(0., self.speed, 0.)
+
+    fn recalculate_ray_directions(&mut self) {
+        self.ray_directions.resize(
+            (self.viewport_width * self.viewport_height) as usize,
+            Vec3A::ZERO,
+        );
+
+        // converting normalized -1 to 1 directions into worldspace directions
+        for y in 0..self.viewport_height {
+            let y_coord = (y / self.viewport_height) as f32;
+            for x in 0..self.viewport_width {
+                let x_coord = (x / self.viewport_width) as f32;
+
+                // normalized between -1 and 1
+                let normalized_coord = vec2(x_coord, y_coord) * 2.0 - Vec2::ONE;
+
+                let target = self.inverse_projection
+                    * vec4(normalized_coord.x, normalized_coord.y, 1.0, 1.0);
+                let world_space_target = self.inverse_view * target;
+
+                let mut ray_direction: Vec3A = world_space_target.truncate().into();
+                ray_direction = (ray_direction / world_space_target.w).normalize();
+
+                // caching the ray directions so we dont need to calculate them every frame
+                self.ray_directions[(x + y * self.viewport_width) as usize] = ray_direction;
+            }
+        }
+    }
+
+    fn on_resize(&mut self, width: u32, height: u32) {
+        if width == self.viewport_width && height == self.viewport_height {
+            return;
+        }
+
+        self.viewport_width = width;
+        self.viewport_height = height;
+
+        self.recalculate_projection();
+        self.recalculate_ray_directions();
     }
 }
